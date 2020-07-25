f2bc5ff653cb03bad621c0be4712cc76bdb15ec7
Daniel Bostock <danielbostock@outlook.com>
2020-07-25 20:48:53 +1000

    modified to loop over until conditionals met

--- import_builder.py
+++ import_builder.py
@@ -5,7 +5,7 @@ sel_err = 'Selection Error: Please select a supported choice by chosing its resp
 
 
 # This will gather from the end user what supported file type that they will be using for importing
-def import_selector ():
+def selector ():
     selection = 0
     while selection == 0:
         selection = int(input('''
@@ -18,45 +18,41 @@ Please select a file type that is required: '''))
         
 
         if selection == 1:
-            csv_file = str(input('Enter the full path location to the CSV file to use for importing of objects: '))
-
-            if selection == 1:
-                confirmation = 0
-                print(' File selected for importing: ' + csv_file)
-                confirmation = int(input(''' Confirm this file location is correct.
+            file = str(input('Enter the full path location to the CSV file to use for importing of objects: '))
+            confirmation = 0
+            selection = 1
+            while confirmation == 0 and selection == 1:
+                if confirmation == False: 
+                    print('File selected for importing: ' + file)
+                    confirmation = int(input('''Confirm this file location is correct.
 1. Yes | 2. No : '''))
-                if confirmation == 0: 
-                    print (sel_err)
-                    selection = 0
-                    break
 
-                if confirmation == 1:
-                    continue
+                    if confirmation == 1:
+                        continue
+                    
+                    if confirmation >= 2:
+                        selection = 0
+                        confirmation = 0
+                        print (sel_err)
+                        break
 
-                if confirmation == 2:
-                    selection = 0
-                    confirmation = 0
-                    break
-                    return selection, confirmation
-                
-                elif confirmation >= 3:
-                    print (sel_err)
-                    break
+                    else:
+                        print (sel_err)
+                        selection = 0
+                        confirmation = 0
+                        break
 
-
-        elif selection == 1 and confirmation == 1:
-            csv.DictReader(open(csv_file))
-            return csv_file
-            continue
-
-        elif selection <= 0:
-            selection = 0
-            print (sel_err)
+        while selection >= 2:
+            if selection >= 2:
+                selection = 0
+                print (sel_err)
+                break
         
-        elif selection >= 2:
-            selection = 0
-            print (sel_err)
-
         else:
             print (sel_err)
             break
+
+    print (selection)
+    return file, confirmation, selection
+
+
