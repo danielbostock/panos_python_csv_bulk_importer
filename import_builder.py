129eef9ad055c000853304bf56141efba0eb45d7
Daniel Bostock <53065247+danielbostock@users.noreply.github.com>
2020-07-30 11:41:38 +1000

    corrected duplication of code and typos

--- import_builder.py
+++ import_builder.py
@@ -1,55 +1,8 @@
 import csv
 
-
 # Error Messages
 sel_err = 'Selection Error: Please select a supported choice by chosing its respective number.'
 gen_err = 'You have either entered an incorrect expected output or something else has broken, please restart the script.'
-
-# This will gather from the end user what supported file type that they will be using for importing
-def selector ():
-    selection = 0
-    while selection == 0:
-        selection = int(input('''
-
-PanOS Bulk Importer currently supports the following file types
-
-1. CSV
-
-Please select a file type that is required: '''))
-        
-
-        if selection == 1:
-            file = str(input('Enter the full path location to the CSV file to use for importing of objects: '))
-            confirmation = 0
-            selection = 1
-            while confirmation == 0 and selection == 1:
-                if confirmation == 0: 
-                    print('File selected for importing: ' + file)
-                    confirmation = int(input('''Confirm this file location is correct.
-1. Yes | 2. No : '''))
-
-                    if confirmation == 1:
-                        continue
-                    
-                    if confirmation >= 2:
-                        selection = 0
-                        confirmation = 0
-                        print (sel_err)
-                        break
-
-            return confirmation, selection
-
-        while selection >= 2:
-            if selection >= 2:
-                selection = 0
-                print (sel_err)
-                break
-        
-        else:
-            print (sel_err)
-            break
-
-    print (selection)
 
 
 # Device info gatherer
@@ -162,8 +115,3 @@ Please select a file type that is required: '''))
             
     
     return file
-
-
-
-
-
